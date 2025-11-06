4e3e788ddc5e24ec3ccb8b43e718024bed545ef3
Pedro <pedrohdelw@gmail.com>
2025-11-05 22:52:33 -0300

    Cria o modelo de dados ResultadoJogo

---
+++ UltimoJogoGremio/Models/ResultadoJogo.cs
@@ -0,0 +1,11 @@
+﻿namespace UltimoJogoGremio.Models
+{
+    public class ResultadoJogo
+    {
+        public string Adversario { get; set; }
+        public string Placar { get; set; }
+        public string DataJogo { get; set; }
+        public string Local { get; set; }
+        public string ResultadoGremio { get; set; }
+    }
+}
