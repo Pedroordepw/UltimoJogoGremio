deac435e3ccee6287e37250f59d37c5194a3c4d2
Pedro <pedrohdelw@gmail.com>
2025-11-05 23:21:28 -0300

    Configura o ServicoPlacarGremio com a autenticação da API

---
+++ UltimoJogoGremio/Services/ServicoPlacarGremio.cs
@@ -0,0 +1,12 @@
+﻿namespace UltimoJogoGremio.Services
+{
+    public class ServicoPlacarGremio
+    {
+        private HttpClient _httpClient;
+        public ServicoPlacarGremio(HttpClient clienteHttp)
+        {
+            _httpClient = clienteHttp;
+            _httpClient.DefaultRequestHeaders.Add("X-Auth-Token", "bf9ff51b6a5548ca8be0c55c2f40188e");
+        }
+    }
+}
